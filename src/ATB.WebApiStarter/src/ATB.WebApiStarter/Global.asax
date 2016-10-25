5086c349a937c24316b92a9eb2412546d678b1e5
Leonardo Lana <leolucas.lana@hotmail.com>
2016-10-25 08:59:05 -0200

    Upload da solution base.

---
+++ src/ATB.WebApiStarter/src/ATB.WebApiStarter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATB.WebApiStarter.Global" Language="C#" %>
---
+++ src/ATB.WebApiStarter/test/ATB.WebApiStarter.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ATB.WebApiStarter.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
